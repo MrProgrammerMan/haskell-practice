315d10b530104a5ef29850f9ab9e70a587cceb97
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-19 16:17:57 +0200

    feat(chapter 3): add tasks

--- test.hs
+++ test.hs
@@ -46,6 +46,8 @@ double x = x + x
 
 quadruple x = double (double x)
 
+quadrupleAlt = double . double
+
 factorial n = Main.product [1..n]
 
 -- 2.2
@@ -73,3 +75,51 @@ init3 = reverse . (drop 1) . reverse
 
 initAlt [x] = []
 initAlt (x:xs) = x : initAlt xs
+
+-- 3.1
+a = ['a','b','c'] :: [Char]
+b = ('a','b','c') :: (Char,Char,Char)
+c = [(False,'0'), (True,'1')] :: [(Bool,Char)]
+d = ([False,True], ['0','1']) :: ([Bool],[Char])
+e = [tail,Prelude.init,reverse] :: [[a] -> [a]]
+
+-- 3.2
+bools :: [Bool]
+bools = [True, True, False]
+
+nums :: [[Int]]
+nums = [[1,2,3],[4,7],[88,9]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a,a)
+copy x = (x,x)
+
+apply :: (a -> b) -> a -> b
+apply f x = f x
+
+-- 3.3
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a,b) -> (b,a)
+swap (x,y) = (y,x)
+
+pair :: a -> b -> (a,b)
+pair x y = (x,y)
+
+double2 :: Num a => a -> a
+double2 x = x*2
+
+palindrome :: [Char] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
+-- 3.4
+-- Checked
+
+-- 3.5
+-- For most types a, there are infinitely many functions that return an a. To check this equality, you would need to check every possible argument. In the case that the return type is a singleton, or the parameter type is simple enough to check all inputs, you could technically define equality.
